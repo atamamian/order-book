1194a794dabeca2bff54b71887af56270ebde23c
Andre Tamamian <andretamamian@Andres-MacBook-Pro.local>
2021-08-08 13:45:16 -0700

    feat(atob-3): setup emotion library

--- src/components/containers/App.tsx
+++ src/components/containers/App.tsx
@@ -1,5 +1,12 @@
 import { FC, ReactElement } from 'react';
 
-const App: FC = (): ReactElement => <div data-testid="headline">Order Book</div>;
+const App: FC = (): ReactElement => (
+  <div
+    css={{ color: 'purple' }}
+    data-testid="headline"
+  >
+    Order Book
+  </div>
+);
 
 export default App;
